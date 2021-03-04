e61c9cbdc31bdc9946721ebfdc8568477a1f5e32
hotire <gngh0101@gmail.com>
2021-03-05 00:38:08 +0900

    Simplify application

--- src/main/kotlin/com/github/hotire/spring/crawler/Application.kt
+++ src/main/kotlin/com/github/hotire/spring/crawler/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringCrawlerApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<SpringCrawlerApplication>(*args)
+    runApplication<Application>(*args)
 }

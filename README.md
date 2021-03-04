c6329057e1442d0f8c2abdcd8bd673963651e276
hotire <gngh0101@gmail.com>
2021-03-05 00:41:38 +0900

    Add jsoup dependency

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Spring-Crawler
+
+
+### Parsing HTML in Java with Jsoup
+
+- https://www.baeldung.com/java-with-jsoup
--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
+    implementation("org.jsoup:jsoup:1.11.3")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
 }
